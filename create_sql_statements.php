aa416f698dd0bec8eb277a49f422d643f7710ccf
denishun01 <slim96@my.bcit.ca>
2021-01-25 14:35:40 -0800

    Revised title

--- create_sql_statements.php
+++ create_sql_statements.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TWD-26-PHP Project</title>
+    <title>PHP Project</title>
     <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
